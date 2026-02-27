c77cb5d133c97c673207f269a8bbe54b2f8afcda
Bharath <kammalabharath96@gmail.com>
2026-02-28 01:20:40 +0530

    Code Modification

--- PrimeNumbersGenerator/PrimeNumbers/Program.cs
+++ PrimeNumbersGenerator/PrimeNumbers/Program.cs
@@ -10,44 +10,44 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-        //    //int n = 9;
-           
-
-        //    ///print butterfly pattern
+            //int n = 9;
 
 
-        //    for (int i = 0; i < n + 1; i++)
-        //    {
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            Console.Write("*");
-        //        }
-        //        for (int k = 0; k < 2 * (n - i); k++)
-        //        {
-        //            Console.Write(" ");
-        //        }
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            Console.Write("*");
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //    for (int i = n - 1; i >= 0; i--)
-        //    {
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            Console.Write("*");
-        //        }
-        //        for (int k = 0; k < 2 * (n - i); k++)
-        //        {
-        //            Console.Write(" ");
-        //        }
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            Console.Write("*");
-        //        }
-        //        Console.WriteLine();
-        //    }
+            ///print butterfly pattern
+
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = 0; k < 2 * (n - i); k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = 0; k < 2 * (n - i); k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

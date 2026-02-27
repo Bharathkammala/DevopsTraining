efef58484b3fccc7185740361f4b3591b9e0d931
Bharath <kammalabharath96@gmail.com>
2026-02-28 01:14:00 +0530

    Intial Commit to testing (to check workflow will trigger or not)

--- PrimeNumbersGenerator/PrimeNumbers/Program.cs
+++ PrimeNumbersGenerator/PrimeNumbers/Program.cs
@@ -10,32 +10,8 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-
-           // Console.Write("Enter number: ");
-           // int n = int.Parse(Console.ReadLine());
             int n = 9;
-            for (int i = 2; i <= n; i++)
-            {
-                bool isPrime = true;
-
-                if (i > 2 && i % 2 == 0)
-                    isPrime = false;
-                else
-                {
-                    for (int j = 3; j * j <= i; j += 2)
-                    {
-                        if (i % j == 0)
-                        {
-                            isPrime = false;
-                            break;   
-                        }
-                    }
-                }
-
-                if (i == 2 || isPrime)
-                    Console.Write(i + " ");
-            }
-
+           
 
             ///print butterfly pattern
 
